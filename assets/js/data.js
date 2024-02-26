69894d4ed645a8658927a270ce0aaeb5c2197d7f
NTQ-SOLUTION\vinh.dao <vinh.dao@ntq-solution.com.vn>
2024-02-26 16:03:10 +0700

    init file

---
+++ assets/js/data.js
@@ -0,0 +1,122 @@
+const englishVocData = [
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Microwave (noun): /ˈmaɪkrəˌweɪv/',
+        sound: './Sounds/microwave.mp3',
+        image: './assets/images/microwave.jpg',
+    },
+    {
+        text: 'Dishwasher (noun): /ˈdɪʃˌwɒʃər/ ',
+        sound: './Sounds/dishwasher.mp3',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    },
+    {
+        text: 'Refrigerator (noun): /rɪˈfrɪdʒəreɪtər/',
+        sound: './Sounds/refrigerator.ogg',
+        image: './assets/images/refrigerator.jpg',
+    }
+]
